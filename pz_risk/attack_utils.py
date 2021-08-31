ca60a1f1a82cc7c745da248f8a9ce74afb973adf
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-08-31 16:44:52 +0900

    add log for rolling dice

--- pz_risk/attack_utils.py
+++ pz_risk/attack_utils.py
@@ -1,6 +1,6 @@
 # https://github.com/rmcarthur/gym-risk/blob/master/gym_risk/attack_utils.py
 
-import logging
+from loguru import logger
 import random
 import numpy as np
 
@@ -14,8 +14,8 @@ attack_until_threshold = 2
 def single_roll(attack: int, defend: int) -> (int, int):
     attack_roll = np.sort(rng.integers(1, sided_die + 1, min(attack_max, attack)))[::-1]
     defend_roll = np.sort(rng.integers(1, sided_die + 1, min(defend_max, defend)))[::-1]
-    logging.info(f"Attack roll: {attack_roll}")
-    logging.info(f"defend roll: {defend_roll}")
+    # logger.debug(f"Attack roll: {attack_roll}")
+    # logger.debug(f"defend roll: {defend_roll}")
     max_loss = min(len(attack_roll), len(defend_roll))
     attack_wins = np.sum([i > j for i, j in zip(attack_roll, defend_roll)])
     attack_loss = max_loss - attack_wins
