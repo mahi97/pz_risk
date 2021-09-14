c4ff05a389b8d3de66213b8edf78bc38d4cdf5e7
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-09-14 13:42:42 +0900

    add arena to challenge different agents in face of eachother

---
+++ pz_risk/arena.py
@@ -0,0 +1,28 @@
+from risk_env import env
+from agents.greedy import GreedyAgent
+from agents.random import RandomAgent
+from loguru import logger
+
+e = env()
+e.reset()
+
+players = [GreedyAgent(i) for i in range(2)]
+players += [RandomAgent(2 + i) for i in range(4)]
+winner = -1
+for agent in e.agent_iter():
+    obs, rew, done, info = e.last()
+    if done:
+        continue
+    e.step(players[agent].act(obs))
+    for a in e.possible_agents:
+        e.unwrapped.land_hist[a].append(len(e.unwrapped.board.player_nodes(a)))
+        e.unwrapped.unit_hist[a].append(e.unwrapped.board.player_units(a))
+        e.unwrapped.place_hist[a].append(e.unwrapped.board.calc_units(a))
+    if all(e.dones.values()):
+        winner = agent
+        break
+    e.render()
+e.render()
+plt.show()
+logger.info('Done in {} Turns and {} Moves. Winner is Player {}'
+            .format(e.unwrapped.num_turns, e.unwrapped.num_moves, winner))
